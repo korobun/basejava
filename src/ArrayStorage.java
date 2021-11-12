fe57bc8197a9a06f8045e1a75c400dfb198adb83
korobun <59797486+korobun@users.noreply.github.com>
2021-11-12 21:27:52 +0300

    added a field storageSize and changed the method size in ArrayStorage

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,17 +3,19 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int storageSize = 0;
 
     void clear() {
-        int storageSize = size();
         for (int i = 0; i < storageSize; i++) {
             storage[i] = null;
         }
+        storageSize = 0;
     }
 
     void save(Resume r) {
-        if (size() < storage.length) {
-            storage[size()] = r;
+        if (storageSize < storage.length) {
+            storage[storageSize] = r;
+            storageSize++;
         }
     }
 
@@ -28,7 +30,6 @@ public class ArrayStorage {
 
     void delete(String uuid) {
         int index = 0;
-        int storageSize = size();
 
         for (int i = 0; i < storageSize; i++) {
             if (storage[i].toString() == uuid) {
@@ -42,27 +43,19 @@ public class ArrayStorage {
                 storage[i - 1] = storage[i];
             }
         }
+        storageSize--;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        int storageSize = size();
         Resume[] allValidResumes = new Resume[storageSize];
         System.arraycopy (storage, 0, allValidResumes, 0, storageSize);
         return allValidResumes;
     }
 
     int size() {
-        int size = 0;
-        for (Resume r : storage) {
-            if (r != null) {
-                size++;
-            } else {
-                break;
-            }
-        }
-        return size;
+        return storageSize;
     }
 }
