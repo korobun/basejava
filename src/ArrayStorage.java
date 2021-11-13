5b3f9f609a7e1f2c1a2515e4b7b9b4635f65666b
korobun <59797486+korobun@users.noreply.github.com>
2021-11-13 11:59:48 +0300

    changed method delete in ArrayStorage

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -20,27 +20,20 @@ public class ArrayStorage {
     }
 
     Resume get(String uuid) {
-        for (Resume r : storage) {
-            if (r!= null && r.toString() == uuid) {
-                return r;
+        for (int i = 0; i < size; i++) {
+            if (storage[i].toString() == uuid) {
+                return storage[i];
             }
         }
         return null;
     }
 
     void delete(String uuid) {
-        int index = 0;
-
         for (int i = 0; i < size; i++) {
             if (storage[i].toString() == uuid) {
                 storage[i] = null;
-                index = i;
-
-                if (index < size - 1) {
-                    for (int j = index + 1; j < size; j++) {
-                        storage[j - 1] = storage[j];
-                    }
-                }
+                System.arraycopy(storage, i + 1, storage, i, size - i);
+                storage[size] = null;
                 size--;
                 break;
             }
@@ -52,7 +45,7 @@ public class ArrayStorage {
      */
     Resume[] getAll() {
         Resume[] allValidResumes = new Resume[size];
-        System.arraycopy (storage, 0, allValidResumes, 0, size);
+        System.arraycopy(storage, 0, allValidResumes, 0, size);
         return allValidResumes;
     }
 
